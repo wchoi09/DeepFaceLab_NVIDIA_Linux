a377ede3f04899f4503a4fe04846800dc70a8cec
wchoi09 <54845449+wchoi09@users.noreply.github.com>
2020-03-15 02:33:08 +0900

    Update 1_CentOS_install_dependencies.sh

--- install_script/1_CentOS_install_dependencies.sh
+++ install_script/1_CentOS_install_dependencies.sh
@@ -1,1 +1,15 @@
+#!/bin/bash
 
+# install epel-release
+sudo dnf -y install epel-release
+sudo dnf config-manager --set-enabled PowerTools
+
+# Confirm EPEL installation
+sudo dnf repolist epel
+
+# Update System
+sudo dnf check-update
+sudo dnf -y update
+
+# Install prerequisites for Anaconda3
+sudo dnf -y install libXcomposite libXcursor libXi libXtst libXrandr alsa-lib mesa-libEGL libXdamage mesa-libGL libXScrnSaver
