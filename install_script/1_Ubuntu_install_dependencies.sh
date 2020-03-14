1f42ba9fa1a569c5fd9b691b50107ae45580810b
jwchoi09 <jwchoi09@yonsei.ac.kr>
2020-03-15 02:35:42 +0900

    Update 1_Ubuntu_install_dependencies.sh

--- install_script/1_Ubuntu_install_dependencies.sh
+++ install_script/1_Ubuntu_install_dependencies.sh
@@ -1,1 +1,8 @@
+#!/bin/bash
 
+# Update System
+sudo apt update
+sudo apt -y dist-upgrade
+
+# Install prerequisites for Anaconda3
+sudo apt -y install libgl1-mesa-glx libegl1-mesa libxrandr2 libxrandr2 libxss1 libxcursor1 libxcomposite1 libasound2 libxi6 libxtst6
