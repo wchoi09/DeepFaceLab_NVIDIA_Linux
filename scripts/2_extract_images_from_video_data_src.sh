111c722ee932d9ac9fdc86ae7f9ec6695f651c1d
jwchoi09 <jwchoi09@yonsei.ac.kr>
2020-03-16 03:20:20 +0900

    Delete 2_extract_images_from_video_data_src.sh

--- scripts/2_extract_images_from_video_data_src.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-source env.sh
-
-$DFL_PYTHON "$DFL_SRC/main.py" videoed extract-video \
-    --input-file "$DFL_WORKSPACE/data_src.*" \
-    --output-dir "$DFL_WORKSPACE/data_src"
-
