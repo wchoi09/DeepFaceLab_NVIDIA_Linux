d3156428bdbb2fb615d313c9de62d82d7c9c3244
jwchoi09 <jwchoi09@yonsei.ac.kr>
2020-03-16 03:21:56 +0900

    Delete 5.3_data_dst_mask_editor.sh

--- scripts/5.3_data_dst_mask_editor.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-source env.sh
-
-$DFL_PYTHON "$DFL_ROOT/main.py" labelingtool edit_mask \
-    --input-dir "$DFL_WORKSPACE/data_dst/aligned" \
-    --confirmed-dir "$DFL_WORKSPACE/data_dst/aligned_confirmed" \
-    --skipped-dir "$DFL_WORKSPACE/data_dst/aligned_skipped"
-
