71be2be92a5a98465bef3d48bbab4100e8f6667a
wchoi09 <54845449+wchoi09@users.noreply.github.com>
2020-03-16 02:46:08 +0900

    Update 5.3_data_dst_mask_editor.sh
    type error

--- scripts/5.3_data_dst_mask_editor.sh
+++ scripts/5.3_data_dst_mask_editor.sh
@@ -2,7 +2,7 @@
 source env.sh
 
 $DFL_PYTHON "$DFL_ROOT/main.py" labelingtool edit_mask \
-    --input-dir "$DFL_WORKSPACE/data_dst/aligned"
+    --input-dir "$DFL_WORKSPACE/data_dst/aligned" \
     --confirmed-dir "$DFL_WORKSPACE/data_dst/aligned_confirmed" \
     --skipped-dir "$DFL_WORKSPACE/data_dst?aligned_skipped"
 
