c1f8f0aaf644e3d18e5f4aafb2ae56b9e69057ff
wchoi09 <54845449+wchoi09@users.noreply.github.com>
2020-03-16 02:18:47 +0900

    Add files via upload

---
+++ scripts/5_data_dst_extract_whole_face_S3FD_+_manual_fix.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+source env.sh
+
+$DFL_PYTHON "$DFL_ROOT/main.py" extract \
+    --input-dir "$DFL_WORKSPACE/data_dst" \
+    --output-dir "$DFL_WORKSPACE/data_dst/aligned" \
+    --detector s3fd \
+    --output-debug \
+    --manual-fix
+    --face-type whole_face
+
