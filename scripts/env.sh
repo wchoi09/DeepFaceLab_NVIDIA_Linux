417a58c99d86a5f9022f34d64ec9be0c48496dbc
wchoi09 <54845449+wchoi09@users.noreply.github.com>
2020-05-20 04:27:01 +0900

    Update env.sh

--- scripts/env.sh
+++ scripts/env.sh
@@ -1,10 +1,20 @@
 source ~/anaconda3/etc/profile.d/conda.sh 2> /dev/null
 conda activate deepfacelab 2> /dev/null
 
+# ========== BASE ENV ==========
 export INTERNAL=$(cd .. && cd _internal && pwd)
+
+
+# ========== PYTHON ENV ==========
 export DFL_PYTHON="python3.6"
+
+# ========== CUDA ENV ==========
+
+# ========== ADDITIONAL ENV ==========
 export DFL_WORKSPACE="$INTERNAL/../workspace/"
 export DFL_ROOT="$INTERNAL/DeepFaceLab"
+export TF_MIN_REQ_CAP=30
+
 
 if [ ! -d "$DFL_WORKSPACE" ]; then
     mkdir -p "$DFL_WORKSPACE"
